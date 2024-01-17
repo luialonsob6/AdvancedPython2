bfcfc1a74a2dc1c5810ed8a90cdc1fb3398451e9
luialonsob6 <luis.alonso6@alumni.esade.edu>
2024-01-17 21:10:55 +0100

    Corrections homework

--- scripts/homework.py
+++ scripts/homework.py
@@ -7,31 +7,36 @@ import click
 
 df = pd.read_csv("FilmGenreStats.csv")
 
+
 @click.command(short_help="parser to import dataset")
 @click.option("-g", "--genre", required=True, help="Genre to filter")
 @click.option("-y", "--year", required=True, help="Year to filter")
 
-
 def filter_genre(genre):
-        """
-        filter by genre
-        """
-        return df[df["Genre"]== genre]
+    """
+    filter by genre
+    """
+    df = df[df["Genre"] == genre]
+    return df
+
 
 def filter_year(year):
-        """
-        Filter by year
-        """
-        return df[df["Year"]== year]
+    """
+    Filter by year
+    """
+    df = df[df["Year"] == year]
+    return df 
 
 def filter_higher_mean():
-        """
-        Filter by higher revenue than mean
-        """
-        mean = df["Gross"].mean()
-        return df[df["Gross"]>mean]
+    """
+    Filter by higher revenue than mean
+    """
+    mean = df["Gross"].mean()
+    df = df[df["Gross"] > mean]
+    return df
+
 
 print(df.head())
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
--- tests/test_filtering.py
+++ tests/test_filtering.py
@@ -1,31 +1,38 @@
+"""
+Test for Filtering functions
+"""
 import unittest
-import pandas as pd
-from scripts.homework_c4 import (filter_genre,filter_year,filter_higher_mean)
-
-df = pd.read_csv("FilmGenreStats.csv")
+from scripts.homework import filter_genre, filter_year, filter_higher_mean
 
 class TestFilteringFunctions(unittest.TestCase):
+    """
+    Class to test the filtering functions
+    """
     def setUp(self):
-        self.path = "datasets/FilmGenreStats.csv"
-        pass
+        self.path = "scripts/FilmGenreStats.csv"
 
     def test_filter_genre(self):
-
+        """
+        Filtering by genre
+        """
         df = filter_genre("Action")
         self.assertTrue(all(df["Genre"] == "Action"))
 
     def test_filter_year(self):
-
+        """
+        Filtering by year
+        """
         df = filter_year(2010)
         self.assertTrue(all(df["Year"] == 2010))
 
     def test_filter_higher_mean(self):
-
+        """
+        Filtering by gross higher than mean.
+        """
         df = filter_higher_mean()
         mean = df["Gross"].mean()
         self.assertTrue(all(df["Gross"] > mean))
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
